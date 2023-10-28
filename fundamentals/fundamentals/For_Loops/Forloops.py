831a000cddacfeff393cd7862ded3b265f0156cf
MedAliFD <medalifaddaoui@gmail.com>
2023-10-28 16:47:48 +0100

    Forloops

---
+++ fundamentals/fundamentals/For_Loops/Forloops.py
@@ -0,0 +1,49 @@
+#------------------------------------------------------------#
+# --- to print needed task only remove the triple quotes" ---#
+#------------------------------------------------------------#
+
+
+#Print 0 to 150
+
+""" for i in range(0,151):
+    print(i) """
+
+# Multiple of five
+
+""" for i in range(2,201): 
+    x = i * 5 
+    print (x) """
+
+#Counting, the Dojo Way
+
+""" for i in range (1,101):
+    if i % 10 == 0:
+        print("coding dojo")
+    elif i % 5 ==0:
+        print("coding ")
+    else:
+        print(i)  """
+
+
+#Whoa. That Sucker's Huge
+
+""" list1=[]
+for i in range (1,500000):
+    if(i % 2 != 0):
+        list1.append(i)
+x = sum(list1)
+print(x)
+"""
+#Countdown by Fours
+
+""" for i in range(2018,-1,-4):
+    print(i) """
+
+#Flexible Counter
+""" lowNum=3
+highNum=9
+mult=3
+
+for i in range (lowNum,highNum+1):
+    if i % mult ==0:
+        print(i) """
