802f8f352401d7b1421114014b88a785e17138fe
MedAliFD <medalifaddaoui@gmail.com>
2023-11-04 13:43:24 +0100

    chainning  methodes

--- fundamentals/oop/main.py
+++ fundamentals/oop/main.py
@@ -7,9 +7,11 @@ class User:
 #make deposit methode
     def make_deposit(self,amount):
         self.account_balance += amount
+        return(self)
 #make withdrawl mthode
     def make_withdrawl(self,amount):
         self.account_balance-= amount
+        return(self)
 #display methode
     def display(self):
         print(f" User.{self.name}, Balance: ${self.account_balance}")
@@ -26,36 +28,25 @@ user2=User("Ali")
 user3=User("Dali")
 
 #first user make 3 deposits and 1 withdrawl
-user1.make_deposit(300)
-user1.make_deposit(200)
-user1.make_deposit(400)
-user1.make_withdrawl(100)
+user1.make_deposit(300).make_deposit(200).make_deposit(400).make_withdrawl(100).display()
 
 #display balances
-user1.display()
 user2.display()
 user3.display()
 
 # second user make 2 deposit and 2 withdrawls
-user2.make_deposit(200)
-user2.make_deposit(150)
-user2.make_withdrawl(100)
-user2.make_withdrawl(120)
+user2.make_deposit(200).make_deposit(150).make_withdrawl(100).make_withdrawl(120).display()
 #display balances
 user1.display()
-user2.display()
 user3.display()
 
 #third user make 1 deposit and 3 withdrawls
-user3.make_deposit(1000)
-user3.make_withdrawl(250)
-user3.make_withdrawl(150) 
+user3.make_deposit(1000).make_withdrawl(250).make_withdrawl(150) .display()
 #display balances
 user1.display()
 user2.display()
 user3.display()
 
 #transfer money
-user1.transfer_money(user2, 340)
-user1.display()
+user1.transfer_money(user2, 340).display()
 user2.display()
