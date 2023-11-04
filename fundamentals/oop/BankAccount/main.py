29946a7766a9c8b3a976780da067468302275879
MedAliFD <medalifaddaoui@gmail.com>
2023-11-04 23:15:45 +0100

    Bank Account

--- fundamentals/oop/BankAccount/main.py
+++ fundamentals/oop/BankAccount/main.py
@@ -1,32 +1,44 @@
 class BankAccount:
+    accounts = 0
 
-
-    # don't forget to add some default values for these parameters!
     def __init__(self, int_rate, balance): 
         self.int_rate=int_rate
         self.balance= balance
+        BankAccount.accounts+= 1
 
-    
+#methode to calculate number of accounts
+    @classmethod
+    def accountsNumber(cls):
+        print(f" there are {cls.accounts} account(s)  created")
+#deposit
     def deposit(self,amount):
         self.balance += amount
         return(self)
-
+#withdrawl
     def withdrawl(self,amount):
         self.balance-= amount
         return(self)
-
+#display
     def display_account_info(self):
         print(f"Balance: ${self.balance}")
-
+#interest 
     def yield_interest(self):
         if self.balance > 0 :
             self.balance= self.balance * (1+self.int_rate)
         else :
             print(f"can't apply interest on your balance which is {self.balance}$")
         return(self)
+    
 
-account1=BankAccount(0.2,500)
-account2=BankAccount(0.2,1000)
+#print all instancesinfo
+        # solution not found yet
+
+
+#Assignemnt tasks
+account1=BankAccount(0.2,500) # initialize the 1st account
+account2=BankAccount(0.2,1000) # initialize the 1st account
+
 account1.deposit(1000).deposit(800).deposit(300).withdrawl(100).yield_interest().display_account_info()
 account2.deposit(300).deposit(700).withdrawl(100).withdrawl(100).withdrawl(100).withdrawl(100).yield_interest().display_account_info()
+BankAccount.accountsNumber()
 
